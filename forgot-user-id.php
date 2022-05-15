52b1747400b2dc2dad255afc497f20a347720ab5
CarlNunag <101043333+CarlNunag@users.noreply.github.com>
2022-05-15 07:08:38 +0700

    Add files via upload

---
+++ forgot-user-id.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<title>Hideaway Hut | Forgot UserID</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-7 col-lg-5">
+					<div class="wrap">
+					    <div class="w-100">
+									<h3 class="mb-4" style="color: rgb(0, 97, 247); border: none; text-align: center;">Restore your User ID</h3>
+								</div>
+						
+						<div class="login-wrap p-4 p-md-5">
+							<div class="d-flex">
+								
+							</div>
+							<form action="#" class="signin-form">
+								<div class="form-group">
+									<input id="password-field" name="contactnumber" type="text" style="border-color: rgba(0, 97, 247, .5);" class="form-control" autofocus required>
+									<label class="form-control-placeholder" style="color: rgb(0, 97, 247);" for="contactnumber">Enter your mobile number</label>
+								</div>
+								<div class="form-group">
+									<button type="submit" style="color: #ffffff; background: rgba(0, 97, 247, 0.7);" class="form-control btn rounded submit px-3">Restore</button>
+								</div>
+								
+							</form>
+							<p class="text-center">Want a new Customer account? &nbsp; &nbsp; <a href="register-as-customer.php" style="color: rgba(0, 97, 247, 0.7);">Register</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="jquery.min.js"></script>
+	<script src="popper.js"></script>
+	<script src="bootstrap.min.js"></script>
+	<script src="main.js"></script>
+
+</body>
+
+</html>
